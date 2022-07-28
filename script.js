9f427ad3d6c0d9788e24da740b6401f1b31af24f
Marcel Machado <90113965+machadomarcel@users.noreply.github.com>
2022-07-28 12:17:48 -0300

    First modal commitment
    Modal created with CSS, Html and JS.

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('open-modal').addEventListener('click', function(){document.getElementById('overlay').style.display='block';})
+
+document.getElementById('close-modal').addEventListener('click', function(){document.getElementById('overlay').style.display='none';})
